98980efa69faeed2f0ab2a838500011fc7102961
dami0 <dami0@iotek.org>
2014-09-13 19:52:22 +0100

    reworked the decimal handling, need to cope with errors

--- netcalc.c
+++ netcalc.c
@@ -60,14 +60,12 @@ int bin(char *nm) {
 	int out;
 	int cnt;
 
-	for (i = 0, out = 0, cnt = 1; i < 35; i++) {
-
+	for (i = 0, out = 0, cnt = 1; i < 32; i++) {
 		/* error handling */
-		if (nm[i] != '1' && nm[i] != '0' && nm[i] != '.') {
+		if (nm[i] != '1' && nm[i] != '0') {
 			puts("Stop right there criminal scum!");
 			return -1;
-		} else if ((nm[i+1] == '1' && nm[i] == '.' && nm[i-1] == '0')
-			|| (nm[i+1] == '1' && nm[i] == '0')) {
+		} else if (nm[i+1] == '1' && nm[i] == '0') {
 			puts("Invalid netmask; a one followed a zero");
 			return -1;
 		}
@@ -76,7 +74,7 @@ int bin(char *nm) {
 		if (nm[i] == '.') cnt++;
 		if (nm[i] == '1') out++;
 	}
-	if (cnt != 4 || i > 34) {
+	if (i < 32) {
 		puts("Invalid netmask; not enough values");
 		return -1;
 	}
@@ -88,34 +86,24 @@ int dec(char *nm) {
 
 	int out[4];
 	int i, j, k;
-	int dels[5];
-	char tmp[4] = "000";
+	char tmp[4];
 
-	dels[0] = -1;
-	for (i = 0, j = 1; i < 18; i++) {
-		if (nm[i] == '.') {
-			dels[j++] = i;
-		}
-		if (nm[i] == '\0') dels[j] = i;
+	/* turn string into array of ints */
+	for (i = 0, j = 0, k = 0; i < 16; i++) {
+		if (nm[i] == '.' || i == 15) {
+			tmp[j] = '\0';
+			out[k++] = atoi(tmp);
+			j = 0;
+		} else { tmp[j++] = nm[i]; }
 	}
-	if (j != 4) {
-		puts("Invalid netmask; not enough values");
-		return -1;
-	}
-
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 3; j++) tmp[j] = '0';
-		for (k = 2, j = dels[i+1] - 1; j > dels[i]; j--, k--) {
-			tmp[k] = nm[j];
-		}
-		out[i] = atoi(tmp);
-		if (out[i] > 255 || out[i] < 0) {
-			puts("Invalid netmask; value(s) outside of 0-255");
-			return -2;
-		}
+	
+	/* size does matter :( */
+	if (k != 4) {
+		puts("Illegal netmask; not enough values");
 	}
 
-	sprintf(nm, "%08i.%08i.%08i.%08i", to2(out[0]), to2(out[1]),
+	/* format appropriately for handling by next function */
+	sprintf(nm, "%08i%08i%08i%08i", to2(out[0]), to2(out[1]),
 			to2(out[2]), to2(out[3]));
 	puts(nm);
 
@@ -125,9 +113,10 @@ int dec(char *nm) {
 int main(int argc, char **argv) {
 
 	int i;
+	int cidr;
+	long int j;
 	char *mode;
 	char *nmask;
-	int cidr;
 
 	/* initialisations */
 	i = 0;
@@ -143,7 +132,7 @@ int main(int argc, char **argv) {
 	/* act accordingly given previous code */
 	switch (i) {
 		case 0:
-			cidr = 0;
+			cidr = -1;
 			break;
 		case 1:
 			cidr = bin(nmask);
@@ -156,8 +145,15 @@ int main(int argc, char **argv) {
 			break;
 	}
 
-	if (cidr > 0) { printf("%i\n", cidr);
-	} else if (cidr < 1) { return -1;
-	} else if (cidr == 0) puts("Nothing happened.");
+	/* print CIDR notation */
+	if (cidr > -1) { printf("/%i\n", cidr);
+	} else if (cidr < 0) { return -1; }
+
+	/* print out information regarding number of networks */
+	j = (long int) pow(2, cidr);
+	printf("\nNetworks: %li\n", j);
+	j = (long int) pow(2, 32 - cidr);
+	printf("Hosts:    %li\n", j);
+	/* go home for dinner */
 	return 0;
 }
